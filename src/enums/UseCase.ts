810cd3b51d2344182448ec77860682b0175987b9
Ferran Llop <contacto@ferranllop.com>
2023-02-15 15:56:12 +0100

    Add UseCase enum

---
+++ src/enums/UseCase.ts
@@ -0,0 +1,11 @@
+export type UseCase = 'UserCreatesCardUseCase' |
+    'UserGetsUserInfoUseCase' |
+    'UserModifiesCardDataUseCase' |
+    'UserModifiesUserDataUseCase' |
+    'UserReadsACardUseCase' |
+    'UserRemovesAccountUseCase' |
+    'UserRemovesCardUseCase' |
+    'UserSearchesForCardsUseCase' |
+    'UserSignsUpUseCase' |
+    'UserSubscribesToCardUseCase' |
+    'UserUnsubscribesFromCardUseCase'
